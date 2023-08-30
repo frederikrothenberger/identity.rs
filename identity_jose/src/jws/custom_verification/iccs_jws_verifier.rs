2c9c153d0a93fd85a215a61a472c631482b1424a
przydatek@github.com <bartosz.przydatek@dfinity.org>
2023-08-30 11:56:59 +0200

    Require that ICCS JWS contains canister sig pk as JWK.

--- identity_jose/src/jws/custom_verification/iccs_jws_verifier.rs
+++ identity_jose/src/jws/custom_verification/iccs_jws_verifier.rs
@@ -1,6 +1,6 @@
 use super::*;
-use crate::jwk::Jwk;
-use crate::jws::{Decoder, JwsVerifier, SignatureVerificationError, SignatureVerificationErrorKind};
+use crate::jwk::{Jwk, JwkType};
+use crate::jws::{Decoder, JwsValidationItem, JwsVerifier, SignatureVerificationError, SignatureVerificationErrorKind};
 use crate::jwu::decode_b64;
 use candid::Principal;
 use ic_certification::{Certificate, HashTree, LookupResult};
@@ -9,6 +9,7 @@ use ic_verify_bls_signature::verify_bls_signature;
 use serde::{Deserialize, Serialize};
 use serde_bytes::ByteBuf;
 use sha2::{Digest, Sha256};
+use std::ops::Deref;
 
 const IC_STATE_ROOT_DOMAIN_SEPARATOR: &[u8; 14] = b"\x0Dic-state-root";
 const IC_ROOT_KEY_DER_PREFIX: &[u8; 37] = b"\x30\x81\x82\x30\x1d\x06\x0d\x2b\x06\x01\x04\x01\x82\xdc\x7c\x05\x03\x01\x02\x01\x06\x0c\x2b\x06\x01\x04\x01\x82\xdc\x7c\x05\x03\x02\x01\x03\x61\x00";
@@ -184,16 +185,30 @@ fn id_alias_signing_input_hash(signing_input: &[u8]) -> Hash {
   hasher.finalize().into()
 }
 
+fn get_canister_signing_pk(jws: &JwsValidationItem) -> Result<IcCsPublicKey, SignatureVerificationError> {
+  let jws_header = jws.protected_header().ok_or(key_decoding_err("missing JWS header"))?;
+  let jwk = jws_header
+    .deref()
+    .jwk()
+    .ok_or(key_decoding_err("missing JWK in JWS header"))?;
+  if jwk.alg() != Some("IcCs") {
+    return Err(unsupported_alg_err("expected IcCs"));
+  }
+  if jwk.kty() != JwkType::Oct {
+    return Err(unsupported_alg_err("expected JWK of type oct"));
+  }
+  let jwk_params = jwk
+    .try_oct_params()
+    .map_err(|_| key_decoding_err("missing JWK oct params"))?;
+  let pk_der = decode_b64(jwk_params.k.as_bytes()).map_err(|_| key_decoding_err("invalid base64url encoding"))?;
+  IcCsPublicKey::try_from(pk_der.as_slice())
+}
 /// Verifies the specified JWS credential against the given canister signatures' public key
 /// and the root public key.
 // TODO: enable customized path checking via a function-parameter
 // TODO: enable validation of claims
 #[allow(dead_code)]
-pub fn verify_id_alias_jws(
-  credential_jws: &str,
-  iccs_pk_der: &[u8],
-  ic_root_pk_der: &[u8],
-) -> Result<(), SignatureVerificationError> {
+pub fn verify_id_alias_jws(credential_jws: &str, ic_root_pk_der: &[u8]) -> Result<(), SignatureVerificationError> {
   ///// Decode JWS.
   let decoder: Decoder = Decoder::new();
   let jws = decoder
@@ -203,7 +218,7 @@ pub fn verify_id_alias_jws(
     .map_err(|e| invalid_signature_err(&format!("signature parsing error: {}", e)))?;
   let certificate: Certificate = serde_cbor::from_slice(iccs_sig.certificate.as_ref())
     .map_err(|e| key_decoding_err(&format!("certificate parsing error: {}", e)))?;
-  let iccs_pk = IcCsPublicKey::try_from(iccs_pk_der)?;
+  let iccs_pk = get_canister_signing_pk(&jws)?;
 
   ///// Check if root hash of the signatures hash tree matches the certified data in the certificate
   let certified_data_path = [b"canister", iccs_pk.signing_canister_id.as_slice(), b"certified_data"];
@@ -255,48 +270,38 @@ pub fn verify_id_alias_jws(
 #[cfg(test)]
 mod tests {
   use super::*;
-  use crate::jwk::{JwkParams, JwkParamsOct, JwkType};
   use crate::jws::JwsAlgorithm;
-  use crate::jwu::encode_b64;
   use assert_matches::assert_matches;
 
   const ROOT_PK_B64URL: &str = "MIGCMB0GDSsGAQQBgtx8BQMBAgEGDCsGAQQBgtx8BQMCAQNhAK32VjilMFayIiyRuyRXsCdLypUZilrL2t_n_XIXjwab3qjZnpR52Ah6Job8gb88SxH-J1Vw1IHxaY951Giv4OV6zB4pj4tpeY2nqJG77Blwk-xfR1kJkj1Iv-1oQ9vtHw";
-  const CANISTER_SIG_PK_B64URL: &str =
-    "MDwwDAYKKwYBBAGDuEMBAgMsAAoAAAAAAAAAAAEBjlTc3oK5QUORmKtOv2Upa2xeAno4BxFQEbf5UdTI6ec";
-  const ID_ALIAS_CREDENTIAL_JWS: &str = "eyJraWQiOiJkaWQ6aWM6aWktY2FuaXN0ZXIiLCJhbGciOiJJY0NzIn0.eyJpc3MiOiJodHRwczovL2ludGVybmV0Y29tcHV0ZXIub3JnL2lzc3VlcnMvaW50ZXJuZXQtaWRlbml0eSIsIm5iZiI6MTYyMDMyODYzMCwianRpIjoiaHR0cHM6Ly9pbnRlcm5ldGNvbXB1dGVyLm9yZy9jcmVkZW50aWFsL2ludGVybmV0LWlkZW5pdHkiLCJzdWIiOiJkaWQ6d2ViOmNwZWhxLTU0aGVmLW9kamp0LWJvY2tsLTNsZHRnLWpxbGU0LXlzaTVyLTZiZmFoLXY2bHNhLXhwcmR2LXBxZSIsInZjIjp7IkBjb250ZXh0IjoiaHR0cHM6Ly93d3cudzMub3JnLzIwMTgvY3JlZGVudGlhbHMvdjEiLCJ0eXBlIjpbIlZlcmlmaWFibGVDcmVkZW50aWFsIiwiSW50ZXJuZXRJZGVudGl0eUlkQWxpYXMiXSwiY3JlZGVudGlhbFN1YmplY3QiOnsiaGFzX2lkX2FsaWFzIjoiZGlkOndlYjpzMzNxYy1jdG5wNS11Ynl6NC1rdWJxby1wMnRlbS1oZTRscy02ajIzai1od3diYS0zN3pibC10Mmx2My1wYWUifX19.2dn3omtjZXJ0aWZpY2F0ZVkBi9nZ96JkdHJlZYMBgwGDAYMCSGNhbmlzdGVygwJKAAAAAAAAAAABAYMBgwGDAYMCTmNlcnRpZmllZF9kYXRhggNYIG3uU_jutBtXB-of0uEA3RkCrcunK6D8QFPtX-gDSwDeggRYINLM_z_MXakw3sDoSiVB5lhRa0uxUB5w6LQQ5phqBX1gggRYIMULjwe1N6XomH10SEyc2r_uc7mGf1aSadeDaid9cUrkggRYIDw__VW2PgWMFp6mK-GmPG-7Fc90q58oK_wjcJ3IrkToggRYIAQTcQAtnxsa93zbfZEZV0f28OhiXL5Wp1OAyDHNI_x4ggRYINkQ8P9zGUvsVi3XbQ2bs6V_3kAiN8UNM6yPgeXfmArEgwGCBFggNVP2WB1Ts90nZG9hyLDaCww4gbhXxtw8R-poiMET62uDAkR0aW1lggNJgLiu1N2JpL4WaXNpZ25hdHVyZVgwqHrYoUsNvSEaSShbW8barx0_ODXD5ZBEl9nKOdkNy_fBmGErE_C7ILbC91_fyZ7CZHRyZWWDAYIEWCB223o-sI97tc3LwJL3LRxQ4If6v_IvfC1fwIGYYQ9vroMCQ3NpZ4MCWCA6UuW6rWVPRqQn_k-pP9kMNe6RKs1gj7QVCsaG4Bx2OYMBgwJYIHszMLDS2VadioIaHajRY5iJzroqMs63lVrs_Uj42j0sggNAggRYICm0w_XxGEw4fDPoYcojCILEi0qdH4-4Zw7klzdaPNOC";
+  const ID_ALIAS_CREDENTIAL_JWS: &str = "eyJqd2siOnsia3R5Ijoib2N0IiwiYWxnIjoiSWNDcyIsImsiOiJNRHd3REFZS0t3WUJCQUdEdUVNQkFnTXNBQW9BQUFBQUFBQUFBQUVCamxUYzNvSzVRVU9SbUt0T3YyVXBhMnhlQW5vNEJ4RlFFYmY1VWRUSTZlYyJ9LCJraWQiOiJkaWQ6aWM6aWktY2FuaXN0ZXIiLCJhbGciOiJJY0NzIn0.eyJpc3MiOiJodHRwczovL2ludGVybmV0Y29tcHV0ZXIub3JnL2lzc3VlcnMvaW50ZXJuZXQtaWRlbml0eSIsIm5iZiI6MTYyMDMyODYzMCwianRpIjoiaHR0cHM6Ly9pbnRlcm5ldGNvbXB1dGVyLm9yZy9jcmVkZW50aWFsL2ludGVybmV0LWlkZW5pdHkiLCJzdWIiOiJkaWQ6d2ViOmNwZWhxLTU0aGVmLW9kamp0LWJvY2tsLTNsZHRnLWpxbGU0LXlzaTVyLTZiZmFoLXY2bHNhLXhwcmR2LXBxZSIsInZjIjp7IkBjb250ZXh0IjoiaHR0cHM6Ly93d3cudzMub3JnLzIwMTgvY3JlZGVudGlhbHMvdjEiLCJ0eXBlIjpbIlZlcmlmaWFibGVDcmVkZW50aWFsIiwiSW50ZXJuZXRJZGVudGl0eUlkQWxpYXMiXSwiY3JlZGVudGlhbFN1YmplY3QiOnsiaGFzX2lkX2FsaWFzIjoiZGlkOndlYjpzMzNxYy1jdG5wNS11Ynl6NC1rdWJxby1wMnRlbS1oZTRscy02ajIzai1od3diYS0zN3pibC10Mmx2My1wYWUifX19.2dn3omtjZXJ0aWZpY2F0ZVkBi9nZ96JkdHJlZYMBgwGDAYMCSGNhbmlzdGVygwJKAAAAAAAAAAABAYMBgwGDAYMCTmNlcnRpZmllZF9kYXRhggNYIODv9b076dlspbUFOyPWJHgYNK9z1_e5ch1_xbztAGgGggRYINLM_z_MXakw3sDoSiVB5lhRa0uxUB5w6LQQ5phqBX1gggRYIBfmGXVF1WCWPapsKI5MoFLJ55x11hQqSb_sRnrp5hFVggRYIMJ9utEUSVVFIqnKBAQ0yrssMWD36ZH2cUb60yoTOzKAggRYIAL_4M5TY9ONUOV0m4NnJ0sP4qs6Dbmt_TfyJW2VcHCtggRYILRWoDKnWsPosTjq1xLq2WPAg0ONkxqUY8Gr7IJiDAYdgwGCBFggNVP2WB1Ts90nZG9hyLDaCww4gbhXxtw8R-poiMET62uDAkR0aW1lggNJgLiu1N2JpL4WaXNpZ25hdHVyZVgwheosd0fsVnQbYtorM71pkwAG4ENhEI84F_xk7uwBeY_4DlNnMdTHFYpLErOXbuS3ZHRyZWWDAYIEWCDvRfWrF74pofmWJkBxcTtb2rClPh4tQ3qWj25MVh-S64MCQ3NpZ4MCWCA6UuW6rWVPRqQn_k-pP9kMNe6RKs1gj7QVCsaG4Bx2OYMBgwJYIDAugH-BjrALnLxVtfR0ayNY5_9_Vc9oVt-H5hpWFVWXggNAggRYIC0ZLl16DWYaDIXJg88YBHdXKqdVgPyXZaZtE6_LgyZR";
+  const ID_ALIAS_CREDENTIAL_JWS_NO_JWK: &str = "eyJraWQiOiJkaWQ6aWM6aWktY2FuaXN0ZXIiLCJhbGciOiJJY0NzIn0.eyJpc3MiOiJodHRwczovL2ludGVybmV0Y29tcHV0ZXIub3JnL2lzc3VlcnMvaW50ZXJuZXQtaWRlbml0eSIsIm5iZiI6MTYyMDMyODYzMCwianRpIjoiaHR0cHM6Ly9pbnRlcm5ldGNvbXB1dGVyLm9yZy9jcmVkZW50aWFsL2ludGVybmV0LWlkZW5pdHkiLCJzdWIiOiJkaWQ6d2ViOmNwZWhxLTU0aGVmLW9kamp0LWJvY2tsLTNsZHRnLWpxbGU0LXlzaTVyLTZiZmFoLXY2bHNhLXhwcmR2LXBxZSIsInZjIjp7IkBjb250ZXh0IjoiaHR0cHM6Ly93d3cudzMub3JnLzIwMTgvY3JlZGVudGlhbHMvdjEiLCJ0eXBlIjpbIlZlcmlmaWFibGVDcmVkZW50aWFsIiwiSW50ZXJuZXRJZGVudGl0eUlkQWxpYXMiXSwiY3JlZGVudGlhbFN1YmplY3QiOnsiaGFzX2lkX2FsaWFzIjoiZGlkOndlYjpzMzNxYy1jdG5wNS11Ynl6NC1rdWJxby1wMnRlbS1oZTRscy02ajIzai1od3diYS0zN3pibC10Mmx2My1wYWUifX19.2dn3omtjZXJ0aWZpY2F0ZVkBi9nZ96JkdHJlZYMBgwGDAYMCSGNhbmlzdGVygwJKAAAAAAAAAAABAYMBgwGDAYMCTmNlcnRpZmllZF9kYXRhggNYIG3uU_jutBtXB-of0uEA3RkCrcunK6D8QFPtX-gDSwDeggRYINLM_z_MXakw3sDoSiVB5lhRa0uxUB5w6LQQ5phqBX1gggRYIMULjwe1N6XomH10SEyc2r_uc7mGf1aSadeDaid9cUrkggRYIDw__VW2PgWMFp6mK-GmPG-7Fc90q58oK_wjcJ3IrkToggRYIAQTcQAtnxsa93zbfZEZV0f28OhiXL5Wp1OAyDHNI_x4ggRYINkQ8P9zGUvsVi3XbQ2bs6V_3kAiN8UNM6yPgeXfmArEgwGCBFggNVP2WB1Ts90nZG9hyLDaCww4gbhXxtw8R-poiMET62uDAkR0aW1lggNJgLiu1N2JpL4WaXNpZ25hdHVyZVgwqHrYoUsNvSEaSShbW8barx0_ODXD5ZBEl9nKOdkNy_fBmGErE_C7ILbC91_fyZ7CZHRyZWWDAYIEWCB223o-sI97tc3LwJL3LRxQ4If6v_IvfC1fwIGYYQ9vroMCQ3NpZ4MCWCA6UuW6rWVPRqQn_k-pP9kMNe6RKs1gj7QVCsaG4Bx2OYMBgwJYIHszMLDS2VadioIaHajRY5iJzroqMs63lVrs_Uj42j0sggNAggRYICm0w_XxGEw4fDPoYcojCILEi0qdH4-4Zw7klzdaPNOC";
 
-  fn root_and_canister_public_keys_der() -> (Vec<u8>, Vec<u8>) {
-    (
-      decode_b64(ROOT_PK_B64URL).expect("failure decoding canister pk"),
-      decode_b64(CANISTER_SIG_PK_B64URL).expect("failure decoding canister pk"),
-    )
+  fn ic_root_pk_der() -> Vec<u8> {
+    decode_b64(ROOT_PK_B64URL).expect("failure decoding canister pk")
   }
 
   #[test]
   fn should_verify_id_alias_vc_jws() {
-    let (ic_root_pk_der, iccs_pk_der) = root_and_canister_public_keys_der();
-    verify_id_alias_jws(ID_ALIAS_CREDENTIAL_JWS, &iccs_pk_der, &ic_root_pk_der).expect("JWS verification failed");
+    verify_id_alias_jws(ID_ALIAS_CREDENTIAL_JWS, &ic_root_pk_der()).expect("JWS verification failed");
   }
 
   #[test]
-  fn should_not_verify_id_alias_vc_jws_with_wrong_canister_pk() {
-    let (ic_root_pk_der, mut iccs_pk_der) = root_and_canister_public_keys_der();
-    iccs_pk_der[25] = iccs_pk_der[25] + 1; // change the canister id
-    let result = verify_id_alias_jws(ID_ALIAS_CREDENTIAL_JWS, &iccs_pk_der, &ic_root_pk_der);
-    assert_matches!(result, Err(e) if e.to_string().contains("no certified data witness for canister"))
+  fn should_not_verify_id_alias_vc_jws_without_canister_pk() {
+    let result = verify_id_alias_jws(ID_ALIAS_CREDENTIAL_JWS_NO_JWK, &ic_root_pk_der());
+    assert_matches!(result, Err(e) if e.to_string().contains("missing JWK in JWS header"));
   }
 
   #[test]
   fn should_not_verify_id_alias_vc_jws_with_wrong_root_pk() {
-    let (mut ic_root_pk_der, iccs_pk_der) = root_and_canister_public_keys_der();
+    let mut ic_root_pk_der = ic_root_pk_der();
     ic_root_pk_der[IC_ROOT_KEY_DER_PREFIX.len()] = ic_root_pk_der[IC_ROOT_KEY_DER_PREFIX.len()] + 1; // change the root pk value
-    let result = verify_id_alias_jws(ID_ALIAS_CREDENTIAL_JWS, &iccs_pk_der, &ic_root_pk_der);
-    assert_matches!(result, Err(e) if e.to_string().contains("invalid BLS signature"))
+    let result = verify_id_alias_jws(ID_ALIAS_CREDENTIAL_JWS, &ic_root_pk_der);
+    assert_matches!(result, Err(e) if e.to_string().contains("invalid BLS signature"));
   }
 
   #[test]
   fn should_verify_id_alias_via_jws_verifier() {
-    let (_ic_root_pk_der, iccs_pk_der) = root_and_canister_public_keys_der();
     let decoder: Decoder = Decoder::new();
     let jws = decoder
       .decode_compact_serialization(ID_ALIAS_CREDENTIAL_JWS.as_ref(), None)
@@ -306,12 +311,8 @@ mod tests {
       signing_input: Box::from(jws.signing_input()),
       decoded_signature: Box::from(jws.decoded_signature()),
     };
-    let mut iccs_pk_jwk = Jwk::new(JwkType::Oct);
-    iccs_pk_jwk
-      .set_params(JwkParams::Oct(JwkParamsOct {
-        k: encode_b64(iccs_pk_der),
-      }))
-      .expect("failed setting JWK params");
-    IcCsJwsVerifier::verify_iccs(verification_input, &iccs_pk_jwk).expect("JWS verification failed");
+    let jws_header = jws.protected_header().expect("missing JWS header");
+    let jwk = jws_header.deref().jwk().expect("missing JWK in JWS header");
+    IcCsJwsVerifier::verify_iccs(verification_input, &jwk).expect("JWS verification failed");
   }
 }
